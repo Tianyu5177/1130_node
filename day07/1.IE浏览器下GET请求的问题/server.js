6e5108c58d2d122bf8bddbe89387846f749b5a79
tianyu <tianyu@hahagmail.com>
2019-03-20 09:33:30 +0800

    update

---
+++ day07/1.IE浏览器下GET请求的问题/server.js
@@ -0,0 +1,24 @@
+let express = require('express')
+let app = express()
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+
+
+app.get('/demo1',(req,res)=>{
+  let data = req.query
+  console.log(data);
+  console.log(11111)
+  res.send('我收到了你ajax_get请求44444')
+})
+
+app.post('/demo2',(req,res)=>{
+  let data = req.body
+  console.log(data);
+  res.send('我收到了你ajax_post请求')
+})
+
+
+
+app.listen(3000,(err)=>{
+  console.log('ok')
+})
